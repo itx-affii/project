dafc6907a447d5aa3ff5eba140ad1db625546724
Muhammad Aftab <115209877+itx-affii@users.noreply.github.com>
2023-05-08 17:50:28 +0500

    slight

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import DummySection from './component/DummySection';
 import Intro from './component/Intro';
 import Navbar from './component/Navbar';
 import Rainbow from './component/Rainbow';
 
 const App = () => {
   return (
-    <div className='bg-[#EFEFEF]'>
+    <div className='bg-[#F1F1F2]'>
       <Navbar />
       <Rainbow />
       <Intro />
+      <DummySection />
     </div>
 
   );
---
+++ src/component/DummySection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DummySection = () => {
+    return (
+        <div className='h-screen w-screen bg-[#EFEEF0]'>
+
+        </div>
+    )
+}
+
+export default DummySection
